2d5d30d1ab7556cbcbc80415429a8f48935b867a
imanechiki <imanechiki1994@gmail.com>
2024-11-13 01:59:56 +0100

    init commit for the set up of restapi and graphql

--- graphqlapi/config/config.mjs
+++ graphqlapi/config/config.mjs
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL || 'http://172.29.70.181:5000';
+export const AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL || 'http://172.20.117.85:5000';
 export const GRAPHQL_API_PORT = process.env.GRAPHQL_API_PORT || 4000;
-export const REST_API_BASE_URL = process.env.REST_API_BASE_URL || 'http://172.29.70.181:3000';
+export const REST_API_BASE_URL = process.env.REST_API_BASE_URL || 'http://172.20.117.85:3000';
--- restapi/config/config.mjs
+++ restapi/config/config.mjs
@@ -1,7 +1,7 @@
 export const REST_API_PORT = process.env.REST_API_PORT || 3000;
 export const GRAPHQL_API_PORT = process.env.GRAPHQL_API_PORT || 4000;
 export const DATABASE_PATH = process.env.DATABASE_PATH || './database.sqlite';
-export const REST_API_BASE_URL = `http://172.29.70.181:${REST_API_PORT}/api`;
+export const REST_API_BASE_URL = `http://172.20.117.85:${REST_API_PORT}/api`;
  
 export const JWT_SECRET = "H<Ms8.cs>w,q9QX*Zn4JVm";
 export const JWT_EXPIRATION = process.env.JWT_EXPIRATION || '1h'
